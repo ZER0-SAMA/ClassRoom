3628995d52fc2476facc3ee2abd786c27103a35e
FOOTBALLcr7* <nick00senpai@gmail.com>
2022-03-25 05:49:53 +0530

    small changer

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ app.use("/new",newmeeting)
 
 if(process.env.NODE_ENV==='production'){
     app.use(express.static('client/build'));
-    app.get("*", function (req, res) {
+    app.get("*", (req, res)=> {
         res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
       });
 }
