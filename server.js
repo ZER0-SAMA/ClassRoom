0077f5fdc46c1e834a2d57823894ec46da87e01d
FOOTBALLcr7* <nick00senpai@gmail.com>
2022-03-25 06:12:03 +0530

    small changer

--- server.js
+++ server.js
@@ -158,15 +158,15 @@ app.use("/new",newmeeting)
      
 
 if(process.env.NODE_ENV==='production'){
-    // app.use(express.static('client/build'));
-    // app.get('*', (req, res)=> {
-    //     res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
-    //   });
-    app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+    app.use(express.static('client/build'));
+    app.get('*', (req, res)=> {
+        res.sendFile(path.join(__dirname, 'client','build', 'index.html'));
+      });
+//     app.use(express.static(path.resolve(__dirname, "./client/build")));
+// // Step 2:
+// app.get("*", function (request, response) {
+//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+// });
 }
 server.listen(PORT,()=>{
     console.log(`Server is lsitening at ${PORT}`)
